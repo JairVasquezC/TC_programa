0126c1a8bd3a8d2a218fdff7a5415e14e6220c00
JairVC18 <jairvasquez874@gmail.com>
2024-12-03 11:04:46 -0500

    dashboard

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\ChatBotController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +22,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('/chat', [ChatBotController::class, 'index']);
+Route::get('/dashboard/datos', [DashboardController::class, 'obtenerDatosDashboard']);
